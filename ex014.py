25bae9bf2bbab666fb1449377da58521864af710
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-04-10 06:59:41 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,5 +1,7 @@
 # escreva um programa que converta uma temperatura digitada em °C e converta para °F
 
 c = float(input('Digite a temperatura em °C: '))
+
 f = (c * 9/5) + 32
+
 print(f'A temperatura em °F é: {f:.2f}°F')
