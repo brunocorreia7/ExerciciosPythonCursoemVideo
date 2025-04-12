48216c96b10838316d3d5c1e632521d735eb5c34
Bruno Correia <correiadesigner7@gmail.com>
2025-04-12 07:27:57 -0300

    exercicio 16

---
+++ ex016.py
@@ -0,0 +1,4 @@
+# crie um programa que le um numero real qualquer pelo teclado e mostre na tela a sua porção inteira. Exemplo: digite um numero: 6.127, o numero 6.127 tem a parte inteira 6.
+
+num = float(input('digite um numero: '))
+print(f'o numero {num} tem a parte inteira {int(num)}')
