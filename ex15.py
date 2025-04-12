80077c6f2d81afa561663f98b1219b7f1557d348
Bruno Correia <correiadesigner7@gmail.com>
2025-04-12 07:20:42 -0300

    resolvendo problemas com ex15

--- ex15.py
+++
@@ -1,8 +0,0 @@
-# escreva um programa que pergunte a quantidade de KM percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e 0,15 por KM rodado.
-
-dias = float(input('quantos dias alugado: '))
-km = float(input('quantos km rodados: '))
-
-pago = (dias * 60) + (km * 0.15)
-
-print(f'o total a pagar é de R${pago:.2f}') 
