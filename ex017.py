e87c3497478f472291b3a388d7b6abb4dc0fe9f7
Bruno Correia <correiadesigner7@gmail.com>
2025-04-13 07:38:08 -0300

    exercicio 17 adicionado

---
+++ ex017.py
@@ -0,0 +1,11 @@
+# faça um programa que leia o comprimento de um cateto oposto e o comprimento de um cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
+
+import math
+
+cateto_oposto = float(input("Digite o comprimento do cateto oposto: "))
+cateto_adjacente = float(input("Digite o comprimento do cateto adjacente: "))
+
+hipotenusa = math.sqrt(cateto_oposto**2 + cateto_adjacente**2)
+
+print(f"O comprimento da hipotenusa é: {hipotenusa:.2f}")
+
