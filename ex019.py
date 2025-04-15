78db36b508a6f7aa618b2b332835303667d94d78
Bruno Correia <correiadesigner7@gmail.com>
2025-04-15 07:03:10 -0300

    ex 19 adicionado

---
+++ ex019.py
@@ -0,0 +1,14 @@
+# um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+
+alunos = []
+
+for i in range(1, 5):
+    aluno = input(f"Digite o nome do {i}º aluno: ")
+    alunos.append(aluno)
+
+escolhido = random.choice(alunos)
+
+
+print(f"O aluno escolhido foi: {escolhido}")
