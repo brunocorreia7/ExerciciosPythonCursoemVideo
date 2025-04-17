b11c7e73a81384bbd2930a863e5342843567a9c5
Bruno Correia <correiadesigner7@gmail.com>
2025-04-17 07:14:59 -0300

    exercicio 21

---
+++ ex021.py
@@ -0,0 +1,8 @@
+# faça um programa em python que abra e reproduza o audio de um arquivo mp3
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
