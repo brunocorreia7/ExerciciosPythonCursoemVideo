e40e2bc42bd3a3898ec49be637bccb36b0f53c59
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-04-20 07:05:11 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,5 +1,4 @@
 # crie um programa que leia um nome de uma cidade e diga se ela começa ou não com o nome "SANTO"
 
-
 cidade = input('Digite o nome de uma cidade: ').strip()
 print(cidade[:5].upper() == 'SANTO')
