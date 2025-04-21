d8784cd65c2552f2e81cae5e890db31b13177de6
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-04-21 06:55:57 -0300

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,4 +1,5 @@
 # crie um programa que leia o nome de uma pessoa e diga se ela tem "silva" no nome
 
 nome = str(input('Digite o nome de uma pessoa: ')).strip().lower()
+
 print(f'Seu nome tem "Silva"? {"silva" in nome}')
