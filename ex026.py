022a015c78e75bbb2a8d33f23fa5e681069535fc
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-04-23 06:55:44 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -3,6 +3,8 @@
 frase = str(input('Digite uma frase: ')).strip().lower()
 
 print(f'A letra "a" aparece {frase.count("a")} vezes') 
+
 print(f'A letra "a" aparece pela primeira vez na posição {frase.find("a") + 1}')
+
 print(f'A letra "a" aparece pela última vez na posição {frase.rfind("a") + 1}')
 
