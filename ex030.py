6154bcea885c59642be35975f032b675177457c6
Bruno Correia <correiadesigner7@gmail.com>
2025-04-26 07:52:55 -0300

    ex 30 criado

---
+++ ex030.py
@@ -0,0 +1,7 @@
+# crie um programa que leia um numero inteiro e mostre na tela se ele é par ou impar
+
+num = int(input('Digite um número inteiro: '))
+if num % 2 == 0:
+    print(f'O número {num} é par.')
+else:
+    print(f'O número {num} é ímpar.')
