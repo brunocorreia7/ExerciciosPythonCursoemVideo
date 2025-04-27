270c7fb3cc0ebee6f11f1a5fe0640be32cac2f9f
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-04-27 06:40:14 -0300

    Update ex031.py

--- ex031.py
+++ ex031.py
@@ -1,6 +1,7 @@
 # desenvolva um programa que calcule a distancia de uma viagem em km. Calcule o preco da passagem, cobrando R$0,50 por km para viagens de ate 200km e R$0,45 para viagens mais longas.
 
 distancia = float(input('Qual a distancia da sua viagem? '))
+
 print(f'você vai começar uma viagem de {distancia}km.')
 if distancia <= 200:
     preco = distancia * 0.50
