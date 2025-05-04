9067c041c49febf6293f128174d190566278035e
Bruno Correia <correiadesigner7@gmail.com>
2025-05-04 05:26:45 -0300

    ex38 criado

---
+++ ex038.py
@@ -0,0 +1,21 @@
+# escreva um programa que leia dois numeros e compare-os, mostrando na tela uma mensagem:
+# - O primeiro valor é maior 
+# - O segundo valor é maior
+# - Não existe valor maior, os dois são iguais
+
+numeros = []
+
+for c in range(1, 3):
+    n = int(input(f'Digite o {c}° número: '))
+    numeros.append(n)
+
+n1, n2 = numeros
+
+if n1 > n2:
+    print(f'O primeiro valor é maior: {n1}')
+elif n2 > n1:
+    print(f'O segundo valor é maior: {n2}')
+else:
+    print('Não existe valor maior, os dois são iguais')
+        
+        
