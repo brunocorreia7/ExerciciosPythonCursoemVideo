f5d8244c066834e0fea5d748afc8ecc0815ac82e
Bruno Correia <correiadesigner7@gmail.com>
2025-05-05 06:35:05 -0300

    exercicio 39 criado

---
+++ ex039.py
@@ -0,0 +1,18 @@
+# faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a idade dele, se ele ainda vai se alistar ao serviço militar, se é a hora de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+
+atual = date.today().year
+
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print(f'Quem nasceu em {nasc} tem {idade} anos em {atual}.')
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print(f'vc ainda não tem 18 anos, ainda faltam {18 - idade} anos para o alistamento.')
+elif idade > 18:
+    ano = idade - 18
+    print(f'Você já deveria ter se alistado há {idade - 18} anos.')
+    
