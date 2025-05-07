9709510b4c3d6cf57baa7dbdd179a7d0fca11d73
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-05-07 05:32:43 -0300

    Update ex041.py

--- ex041.py
+++ ex041.py
@@ -10,6 +10,7 @@ from datetime import date
 atual = date.today().year
 
 nascimento = int(input('Ano de nascimento: '))
+
 idade = atual - nascimento
 print(f'O atleta tem {idade} anos.')
 if idade <= 9:
