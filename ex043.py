29915e1082ff49617e00fe0d7c504524c269d6cb
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-05-09 06:54:59 -0300

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -7,8 +7,11 @@
 # - Acima de 40: Obesidade mórbida
 
 peso = float(input('Digite seu peso: '))
+
 altura = float(input('Digite sua altura: '))
+
 imc = peso / (altura ** 2)
+
 print(f'Seu IMC é {imc:.1f}')
 if imc < 18.5:
     print('Abaixo do peso')
