8d0600094fde698c5ae34cc002011f9a8a3fb232
Bruno Correia <correiadesigner7@gmail.com>
2025-05-09 06:52:06 -0300

    ex043 criado

---
+++ ex043.py
@@ -0,0 +1,22 @@
+# desenvolva uma logica que leia o peso e a altura de uma pessoa,
+# calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+# - Abaixo de 18.5: Abaixo do peso
+# - Entre 18.5 e 25: Peso ideal
+# - 25 até 30: Sobrepeso
+# - 30 até 40: Obesidade
+# - Acima de 40: Obesidade mórbida
+
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+imc = peso / (altura ** 2)
+print(f'Seu IMC é {imc:.1f}')
+if imc < 18.5:
+    print('Abaixo do peso')
+elif 18.5 <= imc < 25:
+    print('Peso ideal')
+elif 25 <= imc < 30:
+    print('Sobrepeso')
+elif 30 <= imc < 40:
+    print('Obesidade')
+else:
+    print('Obesidade mórbida')
