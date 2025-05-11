5b662966395919b8f20ee6b2d9d952109158bf63
Bruno Correia <correiadesigner7@gmail.com>
2025-05-11 07:20:47 -0300

    ex45

---
+++ ex045.py
@@ -0,0 +1,43 @@
+# crie um programa que faça o computador jogar jokenpô com você.
+from random import randint
+itens = ('pedra', 'papel', 'tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada? '))
+print('-=' * 11)
+print(f'Computador jogou {itens[computador]}')
+print(f'Jogador jogou {itens[jogador]}')
+print('-=' * 11)
+
+if computador == 0:
+    if jogador == 0:
+        print('Empate!')
+    elif jogador == 1:
+        print('Jogador venceu!')
+    elif jogador == 2:
+        print('Computador venceu!')
+    else:
+        print('Jogada inválida!')
+elif computador == 1:
+    if jogador == 0:
+        print('Computador venceu!')
+    elif jogador == 1:
+        print('Empate!')
+    elif jogador == 2:
+        print('Jogador venceu!')
+    else:
+        print('Jogada inválida!')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador venceu!')
+    elif jogador == 1:
+        print('Computador venceu!')
+    elif jogador == 2:
+        print('Empate!')
+    else:
+        print('Jogada inválida!')
+else:
+    print('Jogada inválida!')
