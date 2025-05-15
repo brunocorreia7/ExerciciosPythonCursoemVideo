c435200706c37287734e8b5c2ca86f6356eb4ff5
Bruno Correia <correiadesigner7@gmail.com>
2025-05-15 08:45:12 -0300

    ex 48

---
+++ ex048.py
@@ -0,0 +1,9 @@
+# faça um programa que calcule a soma de todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 a 500.
+
+soma = 0
+cont = 0
+for c in range(1,501,2):
+    if c % 3 == 0:
+        soma += c  
+        cont += 1
+print(f'A soma de todos os {cont} valores solicitados é {soma}.')
