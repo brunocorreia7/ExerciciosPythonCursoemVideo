d2968a8c0cc3a26d933d9f27d47e0d23b453ba97
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-05-18 07:07:08 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -1,4 +1,5 @@
 # desenvolva um programa que leia 6 números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
 soma = 0
 cont = 0
 for c in range(1,7):
@@ -6,4 +7,5 @@ for c in range(1,7):
     if n % 2 == 0:
         soma += n
         cont += 1
+        
 print(f'vc informou {cont} números e a soma dos números pares é {soma}')
