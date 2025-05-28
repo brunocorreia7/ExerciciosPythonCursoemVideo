c637896dd634a45e0be142d5f8944162617ce610
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-05-28 06:28:12 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -1,7 +1,9 @@
 # desenvolva um programa que leia o primeira termo e a razão de uma PA, mostrando os 10 primeiros termos dessa progressão.
+
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 decimo = primeiro + (10 - 1) * razao
+
 for c in range(primeiro, decimo + razao, razao):
     print(c, end='-> ')
 print('Acabou!')
